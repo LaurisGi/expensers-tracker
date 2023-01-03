077c803808970d6793f5455da1e4a87a772b25bb
LaurisGi <laurynas.gipas@gmail.com>
2023-01-03 20:40:11 +0200

    added mysql getrequest

--- app/app.js
+++ app/app.js
@@ -1,8 +1,8 @@
 const cors = require('cors');
 const mysql2 = require('mysql2/promise');
 const mysql = require('mysql2');
-
 const express = require('express');
+
 const app = express();
 
 app.use(cors());
@@ -13,10 +13,33 @@ const connection = mysql.createConnection({
   host: '127.0.0.1',
   user: 'root',
   password: 'tomas1234',
-  database: 'shop',
+  database: 'expenses_tracker',
   port: 3306
 });
 
+// 6 uzduotis mano sprendimas
+// app.get('/expenses', (req, res) => {
+//   const sql = 'SELECT * FROM expenses';
+
+
+//   connection.query(sql, (error, results) => {
+//     if (error) {
+//       console.error(error);
+//       res.sendStatus(500);
+//       return;
+//     }
+
+//     res.json(results);
+//   });
+// });
+
+// 6 uzduotis Martyno sprendimas 
+
+app.get('/expenses', (req, res) => {
+  connection.execute('SELECT * FROM expenses', (err, expenses) => {
+    res.send(expenses);
+  });
+});
 
 app.listen(3000, () => {
     console.log('Server listening on port 3000');
