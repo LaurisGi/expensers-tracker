6bd2efdab8fffc0572955fcd95d3d6208f3f2b1a
LaurisGi <laurynas.gipas@gmail.com>
2023-01-05 18:36:27 +0200

    introduced env variables to react

--- client/src/constants/constants.js
+++ client/src/constants/constants.js
@@ -2,9 +2,6 @@ export const LOGGED_IN_USER = {
     id: 1
 }
 
-export const API_URL = 'http://localhost:8080';
-
-
 // export const LOGGED_IN_USER = {
 //     id: 1
 // }
--- client/src/pages/Expenses/Expenses.jsx
+++ client/src/pages/Expenses/Expenses.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useEffect } from "react"
-import { API_URL, LOGGED_IN_USER } from "../../constants/constants"
+import { LOGGED_IN_USER } from "../../constants/constants"
 import styled from 'styled-components'
 
 const ExpensesList = styled.ul`
@@ -38,8 +38,9 @@ export const Expenses = () => {
     const [expenses, setExpenses] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
 
+
     useEffect(() => {
-        fetch(`${API_URL}/expenses?userId=${LOGGED_IN_USER.id}`)
+        fetch(`${process.env.REACT_APP_API_URL}/expenses?userId=${LOGGED_IN_USER.id}`)
             .then(res => res.json())
             .then(data => {
                 setExpenses(data);
